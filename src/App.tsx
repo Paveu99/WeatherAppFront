3cf60787578b3dff3befdc4fc935d4d096e75d69
Paweł Jarecki <paweljarecki10@gmail.com>
2023-08-05 12:44:12 +0200

    Downloading xlsx files as a report and css part finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.css'
 import {LottieView} from "./views/LottieView";
 import { SearchComp } from './components/search/SearchComp';
 import { MainText } from './views/MainText';
+import {DownloadBttn} from "./components/download/DownloadBttn";
 
 export const App = () => {
     return <>
         <LottieView/>
         <MainText/>
         <SearchComp/>
+        <DownloadBttn/>
         <CitiesList/>
   </>
 }
--- src/components/search/SearchComp.tsx
+++ src/components/search/SearchComp.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/Search.css'
 export const SearchComp = () => {
   return <>
-      <input className="search" type="text"/> <button className="btn">Wyszukaj</button>
+      <input className="btn" type="text"/> <button className="btn">Wyszukaj</button>
     </>
 
 }
