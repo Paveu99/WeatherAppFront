093b0759a946630945cbc0c5d42b2f26cb33d1fe
Paweł Jarecki <paweljarecki10@gmail.com>
2023-08-05 12:58:38 +0200

    Minor changes in a report

--- src/components/download/DownloadBttn.tsx
+++ src/components/download/DownloadBttn.tsx
@@ -20,13 +20,18 @@ export const DownloadBttn = () => {
         refreshCities()
     }, []);
 
+    const today = new Date();
+    const date = today.getFullYear()+'.'+(today.getMonth()+1)+'.'+today.getDate();
+    const time = today.getHours() + "-" + today.getMinutes() + "-" + today.getSeconds();
+    const dateTime = date+' '+time;
+
     const downloadxls = (e: React.MouseEvent, data: any) => {
         e.preventDefault();
         const ws = XLSX.utils.json_to_sheet(data);
         const wb = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
         /* generate XLSX file and send to client */
-        XLSX.writeFile(wb, "Raport pogodowy.xlsx");
+        XLSX.writeFile(wb, `Raport pogodowy ${dateTime}.xlsx`);
     }
     return <p>
         <button
