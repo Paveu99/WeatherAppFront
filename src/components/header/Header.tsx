ffec3f12b6535787f422b9927cb423fc666f9bf4
Paweł Jarecki <paweljarecki10@gmail.com>
2023-08-07 15:04:44 +0200

    Cleanup

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/Header.css'
 export const Header = () => {
+
+  const today = new Date();
+
   return <header className="header">
     <Link className="link" to='/' style={{textDecoration: "none"}}>Strona główna</Link>
     <Link className="link" to='/info' style={{textDecoration: "none"}}>Informacje</Link>
+    <p className="link-time">Godzina ostatniej aktualizacji: <b>{today.toLocaleTimeString()}</b></p>
   </header>
 }
